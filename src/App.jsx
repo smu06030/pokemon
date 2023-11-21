9e72f4c3ba701cf4978b665e790f13067e095b44
incheol <smu06030>
2023-11-21 17:24:26 +0900

    로그인 UI 생성 및 firebase 연동동

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import DetailPage from './pages/DetailPage';
 import LoginPage from './pages/LoginPage';
+import NavBar from './components/NavBar';
 
 const Layout = () => {
   return (
     <>
       <NavBar />
-      <br/>
-      <br/>
-      <br/>
       <Outlet />
     </>
   )
 }
 
 const App = () => {
-
-  localost:4000/login
 
   return (
     <BrowserRouter>
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <section className="bg-gray-50 min-h-[90vh] flex items-center justify-center">
+      <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+        <div className="md:w-1/2 px-8 md:px-16">
+          <h2 className="font-bold text-2xl">Pokemon</h2>
+          <p className="text-xs mt-4 text-[#002D74]">포켓몬 사이트에 오신걸</p>
+          <p className="text-xs mt-4 text-[#002D74]">환영합니다.</p>
+          <p className="text-xs mt-4 text-[#002D74]">로그인해 주세요.</p>
+        </div>
+        <div className="md:block hidden w-1/2">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+            alt="login"
+            className="rounded-2xl"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
